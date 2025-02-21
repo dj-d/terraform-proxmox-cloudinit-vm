097d0f63267967eb06ecd1da99a894ddfff1e2c7
Daniele Albanese <daniele.albanese98@gmail.com>
2025-02-21 21:22:35 +0100

    feat: added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "instance_ip" {
-  value = proxmox_virtual_environment_vm.this.*.initialization.0.ip_config.0.ipv4.0.address
+output "ipv4_addresses" {
+  value = proxmox_virtual_environment_vm.this.ipv4_addresses
+}
+
+output "mac_addresses" {
+  value = proxmox_virtual_environment_vm.this.mac_addresses
 }
